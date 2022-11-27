9551ea297ba5cedcc43343c976000ed09508a763
unknown <quanledang0@gmail.com>
2022-11-27 10:03:38 +0700

    first commit

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
-import React from "react";
-import AppNavigation from "./src/navigation";
+import { StatusBar } from "expo-status-bar";
+import HomeScreen from "./Screens/HomeScreen";
+import { NativeBaseProvider, Text, Box } from "native-base";
+import LoginScreen from "./Screens/LoginScreen";
+import RegisterScreen from "./Screens/RegisterScreen";
+import NotVerifyScreen from "./Screens/NotVerifyScreen";
+import SingleProductScreen from "./Screens/SingleProductScreen";
 export default function App() {
-  return <AppNavigation />;
+  return (
+    <NativeBaseProvider>
+     <SingleProductScreen/>
+    </NativeBaseProvider>
+  );
 }
+
+
